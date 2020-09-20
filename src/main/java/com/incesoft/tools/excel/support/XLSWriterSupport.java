c02274cfa143a59311bb021bd32dd97f63056c1b
cuihq <cuihongqing@umpay.com>
2020-09-20 17:45:26 +0800

    修改deploy仓库;WriteSupport接口增加指定empty.xlsx的open方法

--- src/main/java/com/incesoft/tools/excel/support/XLSWriterSupport.java
+++ src/main/java/com/incesoft/tools/excel/support/XLSWriterSupport.java
@@ -32,115 +32,119 @@ public class XLSWriterSupport extends WriterSupport {
 
 	WritableWorkbook workbook;
 
-	public void open() {
+	public void open( ) {
 		try {
-			if (file != null) {
-				workbook = Workbook.createWorkbook(file);
-			} else if (output != null) {
-				workbook = Workbook.createWorkbook(output);
+			if( file != null ) {
+				workbook = Workbook.createWorkbook( file );
+			} else if( output != null ) {
+				workbook = Workbook.createWorkbook( output );
 			} else {
-				throw new IllegalStateException("no output specified");
+				throw new IllegalStateException( "no output specified" );
 			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
+		} catch( IOException e ) {
+			throw new RuntimeException( e );
 		}
 	}
-
-	private static int getRGB(Colour c) {
-		RGB defaultRGB = c.getDefaultRGB();
-		return defaultRGB.getRed() << 16 | defaultRGB.getGreen() << 8 | defaultRGB.getBlue();
-	}
-
-	public static Colour transformColor(int c) {
-		int result = Collections.binarySearch(colours, c & 0x00ffffff);
-		if (result < 0) {
-			result = -(result + 1);
-			if (result > colours.size() - 1) {
-				result = colours.size() - 1;
-			}
-		}
-		return colourMap.get(colours.get(result));
-	}
-
-	static List<Integer> colours = new ArrayList<Integer>();
-	static Map<Integer, Colour> colourMap = new HashMap<Integer, Colour>();
-	static {
-		for (Colour c : Colour.getAllColours()) {
-			colourMap.put(getRGB(c), c);
-			colours.add(getRGB(c));
-		}
-		Collections.sort(colours);
-	}
-
-	private static final Log log = LogFactory.getLog(XLSWriterSupport.class);
 
 	@Override
-	public void writeRow(String[] rowData, CellFormat[] formats) {
-		for (int col = 0; col < rowData.length; col++) {
+	public void open( String emptyTemplate ) {
+		open( );
+	}
+
+	private static int getRGB( Colour c ) {
+		RGB defaultRGB = c.getDefaultRGB( );
+		return defaultRGB.getRed( ) << 16 | defaultRGB.getGreen( ) << 8 | defaultRGB.getBlue( );
+	}
+
+	public static Colour transformColor( int c ) {
+		int result = Collections.binarySearch( colours, c & 0x00ffffff );
+		if( result < 0 ) {
+			result = -( result + 1 );
+			if( result > colours.size( ) - 1 ) {
+				result = colours.size( ) - 1;
+			}
+		}
+		return colourMap.get( colours.get( result ) );
+	}
+
+	static List<Integer> colours = new ArrayList<Integer>( );
+	static Map<Integer, Colour> colourMap = new HashMap<Integer, Colour>( );
+	static {
+		for( Colour c : Colour.getAllColours( ) ) {
+			colourMap.put( getRGB( c ), c );
+			colours.add( getRGB( c ) );
+		}
+		Collections.sort( colours );
+	}
+
+	private static final Log log = LogFactory.getLog( XLSWriterSupport.class );
+
+	@Override
+	public void writeRow( String[] rowData, CellFormat[] formats ) {
+		for( int col = 0; col < rowData.length; col++ ) {
 			String cellString = rowData[col];
-			if (cellString != null) {
+			if( cellString != null ) {
 				CellFormat format = null;
-				if (formats != null && formats.length > 0) {
-					for (CellFormat cellFormat : formats) {
-						if (cellFormat != null && cellFormat.getCellIndex() == col) {
+				if( formats != null && formats.length > 0 ) {
+					for( CellFormat cellFormat : formats ) {
+						if( cellFormat != null && cellFormat.getCellIndex( ) == col ) {
 							format = cellFormat;
 							break;
 						}
 					}
 				}
-				Label label = new Label(col, rowpos, cellString);
+				Label label = new Label( col, rowpos, cellString );
 				try {
-					sheet.addCell(label);
-				} catch (Exception e) {
-					throw new RuntimeException(e);
+					sheet.addCell( label );
+				} catch( Exception e ) {
+					throw new RuntimeException( e );
 				}
-				if (format != null) {
-					WritableCell c = workbook.getSheet(0).getWritableCell(col, rowpos);
-					WritableCellFormat newFormat = c.getCellFormat() == null ? new WritableCellFormat()
-							: new WritableCellFormat(c.getCellFormat());
-					if (format != null && (format.getBackColor() != -1 || format.getForeColor() != -1)) {
-						if (format.getBackColor() != -1) {
+				if( format != null ) {
+					WritableCell c = workbook.getSheet( 0 ).getWritableCell( col, rowpos );
+					WritableCellFormat newFormat = c.getCellFormat( ) == null ? new WritableCellFormat( ) : new WritableCellFormat( c.getCellFormat( ) );
+					if( format != null && ( format.getBackColor( ) != -1 || format.getForeColor( ) != -1 ) ) {
+						if( format.getBackColor( ) != -1 ) {
 							try {
-								newFormat.setBackground(transformColor(format.getBackColor()));
-							} catch (WriteException e) {
-								log.error("", e);
+								newFormat.setBackground( transformColor( format.getBackColor( ) ) );
+							} catch( WriteException e ) {
+								log.error( "", e );
 							}
 						}
-						if (format.getForeColor() != -1) {
+						if( format.getForeColor( ) != -1 ) {
 							try {
-								WritableFont writableFont = new WritableFont(WritableFont.createFont(format
-										.getFontName()));
-								writableFont.setColour(Colour.PINK2);
-								newFormat.setFont(writableFont);
-							} catch (WriteException e) {
-								log.error("", e);
+								WritableFont writableFont = new WritableFont( WritableFont.createFont( format.getFontName( ) ) );
+								writableFont.setColour( Colour.PINK2 );
+								newFormat.setFont( writableFont );
+							} catch( WriteException e ) {
+								log.error( "", e );
 							}
 						}
 					}
-					c.setCellFormat(newFormat);
+					c.setCellFormat( newFormat );
 				}
 			}
 		}
 	}
 
-	public void writeRow(String[] rowData) {
-		writeRow(rowData, null);
+	public void writeRow( String[] rowData ) {
+		writeRow( rowData, null );
 	}
 
-	public void close() {
+	public void close( ) {
 		try {
-			workbook.write();
-			workbook.close();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
+			workbook.write( );
+			workbook.close( );
+		} catch( Exception e ) {
+			throw new RuntimeException( e );
 		}
 	}
 
-	public void createNewSheet() {
-		sheet = workbook.createSheet("sheet" + sheetIndex + 1, sheetIndex);
+	public void createNewSheet( ) {
+		sheet = workbook.createSheet( "sheet" + sheetIndex + 1, sheetIndex );
 	}
 
-	public int getMaxRowNumOfSheet() {
+	public int getMaxRowNumOfSheet( ) {
 		return 60000;
 	}
+
 }
